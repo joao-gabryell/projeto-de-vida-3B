8589487493ec9a9ca8c68a1ee65fc8c34623f1be
joao-gabryell <163905001+joao-gabryell@users.noreply.github.com>
2025-05-06 16:20:54 -0300

    06/05

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ console.log(botoes);
 
 for(let i=0; i<botoes.length; i++){
 
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classlist.remove("ativo")
+
+}
     botoes[i].onclick = function(){
         botoes[i].classlist.add("ativo")
     }
