295f82eb81096beb33221e25f724c8d42a5727b4
joao-gabryell <163905001+joao-gabryell@users.noreply.github.com>
2025-05-06 16:16:30 -0300

    06/05

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const botoes = document.querySelectorAllok(".botao")
 console.log(botoes);
+
+for(let i=0; i<botoes.length; i++){
+
+    botoes[i].onclick = function(){
+        botoes[i].classlist.add("ativo")
+    }
+
+    }
